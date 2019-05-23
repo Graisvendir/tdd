75b719184c1848f9f482a54e43b7346c36fb5ea8
Graisvendir <graisvendir@gmail.com>
2019-05-23 17:54:14 +0600

    add Pool.add_variant, test is passed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyglet
 
+
 def makeChoice():
     choice = input()
     if isinstance(choice, int):
@@ -7,14 +8,19 @@ def makeChoice():
     else:
         return 0
 
+
 class Pool:
 
     def __init__(self):
         self.variants = []
 
+    def add_variant(self, name: str):
+        self.variants.append(name)
+
     def print(self):
         for i in self.variants:
             print(i)
+
 
 '''
 1) print variants and play sound choice
