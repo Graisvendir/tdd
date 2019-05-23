650ece2883a278b9508e673302b66dd59ec85982
Graisvendir <graisvendir@gmail.com>
2019-05-23 16:39:35 +0600

    add choice is int test, test is not passed

--- testFunctions.py
+++ testFunctions.py
@@ -6,3 +6,7 @@ class TestChoseMusic(unittest.TestCase):
     def test_make_choice_exists(self):
         choice = main.makeChoice()
         self.assertIsNotNone(choice, 'makeChoice func must be existing')
+
+    def test_choice_is_int(self):
+        choice = main.makeChoice()
+        self.assertTrue(isinstance(choice, int))
