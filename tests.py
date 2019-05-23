4d6bd41b4ca4c458e14aa230b2dab23a348cde9c
Graisvendir <graisvendir@gmail.com>
2019-05-23 16:14:07 +0600

    add music existing test, test is not passed

--- tests.py
+++ tests.py
@@ -4,3 +4,7 @@ import pyglet
 class TestChoseMusic(unittest.TestCase):
     def testPygletNotInstall(self):
         self.assertIsNotNone(pyglet, "pyglet cant be None")
+
+    def testLazzyTownMusic(self):
+        music = pyglet.media.load('music/lazzyTown.mp3')
+        self.assertIsNotNone(music, 'music must exists')
